1251212dab4cf3417d9abc760a4e0355707e96b5
Chris Richardson <chris.richardson@naitive.com>
2022-10-05 15:36:31 +0000

    Fix setup.py by adding package_dir

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from setuptools import setup, find_packages
 # Used for the long_description.  It's nice, because now 1) we have a top level
 # README file and 2) it's easier to type in the README file than to put a raw
 # string in below ...
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
@@ -13,7 +14,8 @@ setup(name = "final_test_package",
       version = "0.1.0",
       author = "Naitive2022",
       description = "A package for doing great things!",
-      long_description=read('README.md'), 
+      long_description=read('README.md'),
+      package_dir = {"": "src"},
       packages=find_packages(where='src',  # '.' by default
                              include=['final_test_package*'],  # ['*'] by default
                              exclude=['final_test_package.tests'],  # empty by default
