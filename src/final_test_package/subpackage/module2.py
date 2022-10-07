ed14aca83b5fcfe28c3b8e0c9e4fccce280f047c
Chris Richardson <chris.richardson@naitive.com>
2022-10-07 13:47:41 +0000

    Add func to module2.py AND add module3.py

--- src/final_test_package/subpackage/module2.py
+++ src/final_test_package/subpackage/module2.py
@@ -17,3 +17,23 @@ def func2(a, b):
 
     """
     return a - b
+
+def func22(a, b):
+    """Summary line.
+
+    Extended description of function.
+
+    Parameters
+    ----------
+    a : int
+        An unecessary number
+    b : int
+        An unecessary number
+
+    Returns
+    -------
+    bool
+        Description of return value
+
+    """
+    return a * b
---
+++ src/final_test_package/subpackage/module3.py
@@ -0,0 +1,19 @@
+def func33(a, b):
+    """Summary line.
+
+    Extended description of function.
+
+    Parameters
+    ----------
+    a : int
+        An unecessary number
+    b : int
+        An unecessary number
+
+    Returns
+    -------
+    bool
+        Description of return value
+
+    """
+    return a // b
